f739349cc739d09e57a68174b90a719cd11b14af
Eivind Uggedal <eivind@uggedal.com>
2013-08-09 09:39:52 +0200

    37signals/sub

---
+++ completions/snapback.bash
@@ -0,0 +1,14 @@
+_snapback() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(snapback commands)" -- "$word") )
+  else
+    local command="${COMP_WORDS[1]}"
+    local completions="$(snapback completions "$command")"
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
+}
+
+complete -F _snapback snapback
